48c0257b8d7f11518e64d3a87726e4e55f4aae48
shangkaul <shangkaul@gmail.com>
2021-09-30 19:55:21 +0530

    style home text

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ return(
     <div className="Home">
         <div className="homeContainer">
             <div className="homeText"><h2>Hello, I’m Shivang Kaul</h2>
-            I am a CS undergrad who works on real-world projects. 
+            I am a CS grad who works on real-world projects. 
             I love to work as a backend developer and I am quite proficient 
             in making beautiful frontend for apps and web apps. 
             Machine Learning intrigues me and I am working to 
