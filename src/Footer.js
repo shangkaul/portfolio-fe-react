7f709d8987f10dcdf542c88340469677e5010e67
shangkaul <shangkaul@gmail.com>
2021-09-30 14:31:28 +0530

    feat blogs

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,15 @@
 import "./Footer.css";
-import {FaGithubSquare,FaInstagramSquare,FaLinkedin} from "react-icons/fa";
+import {FaGithubSquare,FaLinkedin} from "react-icons/fa";
 export default function Footer()
 {
     return(
         <div className="Footer">
-            <div className="left">Portfolio @ Shivang</div>
-            <div className="right"><FaGithubSquare style={{margin:"0 1rem"}}/><FaInstagramSquare style={{margin:"0 1rem"}}/><FaLinkedin style={{margin:"0 1rem"}}/></div>
+            <div className="left">Portfolio @ Shivang -  <a style={{color:"white"}} href="https://shangkaul.in">shangkaul.in</a></div>
+            <div className="right">
+                <a style={{color:"white"}} href="https://github.com/shangkaul"><FaGithubSquare style={{margin:"0 1rem"}}/></a>
+                <a style={{color:"white"}} href="https://www.linkedin.com/in/shivang-k-876906110/"><FaLinkedin style={{margin:"0 1rem"}}/></a>
+                
+                </div>
         </div>
     )
 }
--- src/Work.js
+++ src/Work.js
@@ -1,9 +1,51 @@
-import "./Work.css";
+import "./Projects.css";
+import Footer from "./Footer";
 export default function Work()
 {
-    return(
-        <div className="Work">
-            
+
+    var proj=[
+        {
+            head:"Business Tech Solutions Associate - ZS Associates",
+            date:"Aug 2020 - Present",
+            content:"Working on building custom solutions leveraging Data Science algorithms and front end frameworks for clients in the Health Sciences space. Domain expertise in the Medical affairs domain.",
+            link:"https://www.zs.com/",
+    },{
+        head:"Full Stack Mobile Dev - Think Digital",
+        date:"Aug 2016 - 2020",
+        content:"Worked as a Hybrid App developer for Think Digital - college tech club. Worked and managed various full stack development projects using the Ionic Framework for frontend. Other tech - NodeJS, PHP, MySQL",
+        link:"https://think-digital.in/",
+},{
+    head:"Full Stack Dev - Inception Wave",
+    date:"Jan 2019-2020",
+    content:"Worked on building the startup's flagship product - Grapido. A pprofessional socia media network to connect students and early professionals with mentors. Tech Stack - Ionic Framework, Node,Express, MongoDB",
+    link:"https://www.inceptionwave.in/home",
+},
+{
+    head:"Software Intern - DRDO",
+    date:"May 2019",
+    content:"Developed a wireless CCTV solution POC. I was responsible for developing a basic facial recognition interface using openCV. ",
+    link:"https://www.drdo.gov.in/",
+}
+
+    ]
+    return(<div>
+        <div className="Projects">
+               
+               {proj.map((item,i)=>{
+                   return(<div className="cardWork">
+                   <div className="cardHead">{item.head}</div>
+                   <div className="cardDate">{item.date}</div>
+                   <hr style={{width:"40%"}} />
+                   <div className="cardContent">{item.content}</div>
+                   <div className="btnBar"><a href={item.link}><button className="btnProj">Link</button></a></div>
+                   </div>
+
+                   )
+               })}
+                
+
         </div>
+                <Footer/></div>
+
     )
 }
